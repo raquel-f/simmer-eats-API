3d91b9d0da16a53dbed141606d822450b1d325d5
Raquel Ferreira <44173946+raquel-f@users.noreply.github.com>
2023-05-21 18:49:38 +0100

    small change in food pricing storage

--- models/foodItem.js
+++ models/foodItem.js
@@ -4,7 +4,7 @@ import { SERVINGS } from '../constants/index.js';
 const foodSchema = mongoose.Schema({
     name: { type: String, required: true },
     ingredients: { type: [String], required: true },
-    price: { type: Number, required: true },
+    price: { type: [Number], required: true },
     skill: { type: Number, default: 1 },
     serving: { type: [String], default: [SERVINGS.Single], enum: [SERVINGS.Single, SERVINGS.Family, SERVINGS.Party] },
     notes: { type: [String], required: false },
